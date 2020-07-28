14d7d80d6b73c0d8c16f4f7adbafea3d93d067ee
AashishDhakal <aashish.dhakal@student.ku.edu.np>
2020-07-28 18:05:37 +0545

    customized admin panel

--- events/admin.py
+++ events/admin.py
@@ -1,4 +1,13 @@
 from django.contrib import admin
 from .models import Event
 # Register your models here.
-admin.site.register(Event)
+
+
+class EventAdmin(admin.ModelAdmin):
+    list_display = ['event_name', 'event_date', 'event_time', 'sponsor', 'organizer']
+    list_editable = ['event_date', 'event_time']
+    search_fields = ['event_name', ]
+    list_filter = ['sponsor', 'organizer']
+
+
+admin.site.register(Event, EventAdmin)
