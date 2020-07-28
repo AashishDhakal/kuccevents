6901e6230a9d2291cf3cdcb7939e20b885b99d95
AashishDhakal <aashish.dhakal@student.ku.edu.np>
2020-07-28 17:24:58 +0545

    added event list view

--- events/views.py
+++ events/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
+from .models import Event
 
 # Create your views here.
+
+def EventListView(request):
+    all_events = Event.objects.all()
+    return render(request, 'eventlist.html')
